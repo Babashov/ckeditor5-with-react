9a59dc40b3cf5f29de6267ccde4e2d5cb47db86b
Babashov Mahammad <babashov.info@gmail.com>
2023-01-12 19:23:39 +0400

    added plaintText from ckeditor

--- src/components/CkEditor.jsx
+++ src/components/CkEditor.jsx
@@ -1,20 +1,45 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react"
 import Editor from 'ckeditor5-custom-build/build/ckeditor'
+import viewToPlainText from '@ckeditor/ckeditor5-clipboard/src/utils/viewtoplaintext';
+
+import { useState } from "react";
 
 function CkEditor() {
+
+  const [ckValue,setCkValue] = useState('Using CKEditor 5 build in React')
+
+  const [plainTextCkValue,setPlainTextCkValue] = useState(ckValue)
+
   return (
     <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
+                <h2>{plainTextCkValue}</h2>
                 <CKEditor
                     editor={ Editor }
-                    data="<p>Hello from CKEditor 5!</p>"
+                    data={ckValue}
                     onReady={ editor => {
                         // You can store the "editor" and use when it is needed.
                         console.log( 'Editor is ready to use!', editor );
+
+                        editor.editing.view.change((writer) => {
+                          writer.setStyle(
+                              "height",
+                              "300px",
+                              editor.editing.view.document.getRoot()
+                          );
+                          });
                     } }
+
+
                     onChange={ ( event, editor ) => {
-                        const data = editor.getData();
+                        const data = (editor.getData());
+                        const plainData = viewToPlainText(editor.editing.view.document.getRoot())
                         console.log( { event, editor, data } );
+                        console.log(plainData)
+
+
+
+                        setCkValue(data)
+                        setPlainTextCkValue(plainData)
                     } }
                     onBlur={ ( event, editor ) => {
                         console.log( 'Blur.', editor );
@@ -23,7 +48,8 @@ function CkEditor() {
                         console.log( 'Focus.', editor );
                     } }
                 />
-            </div>
+
+    </div>
   )
 }
 
